942da38fe556d2b639c0f0fdcf00cdb2b39ad27e
Huynh Thanh Tam <46307504+tamht298@users.noreply.github.com>
2020-03-11 10:26:41 +0700

    add welcome component

--- front-end-user/src/app/app.module.ts
+++ front-end-user/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
